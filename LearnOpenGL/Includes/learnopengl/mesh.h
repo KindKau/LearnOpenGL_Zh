7d931c2c9ac4171a049cd3ac262c345d70bb4601
liuzhongkai <liuzhongkai@softstar.net.cn>
2019-08-27 13:18:05 +0800

    添加 assimp 库

---
+++ LearnOpenGL/Includes/learnopengl/mesh.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <glad/glad.h> // holds all OpenGL type declarations
+
+#include <glm/glm.hpp> // Math lib
+#include <glm/gtc/matrix_transform.hpp>
+
+#include <learnopengl/shader.h>
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector>
+using namespace std;
